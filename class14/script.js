4ad50a5888405daf3f94c8fc1e6ad8df68a94256
alihaiderdev <ah1675179@gmail.com>
2024-01-28 18:48:09 +0500

    done with class 10 - class 14 code

---
+++ class14/script.js
@@ -0,0 +1,17 @@
+// console.log(10 + 20 / 12);
+// console.log(10 + 20 * 12);
+// console.warn(10 + 20 * 12);
+// console.error(10 + 20 * 12);
+var name = "ali haider's";
+var string = `ali haider's
+
+
+;lkl;klklfdsk
+lmlfkjgjdlkjfdlkglkjgkjglkg`; //string
+var myAge = 24; // number
+var isMale = true;  // boolean
+
+
+console.log('My name is ' + name + ' and my age is ' + myAge);
+console.log(`My name is ${name} and my age is ${myAge} and i am male ${isMale}`);
+console.log('My name is ', string );
